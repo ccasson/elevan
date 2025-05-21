63d437ad034feda59e2978ade8bdd7caadfce1c8
ccasson <colincasson80@gmail.com>
2025-05-21 12:37:41 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#F6E4D0"
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        script: ["Great Vibes", "cursive"]
+      }
+    }
+  },
+  plugins: []
+};
